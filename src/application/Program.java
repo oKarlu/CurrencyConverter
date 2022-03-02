1ef2393668eaeac5eea4fc688be10d332db37d82
Carlos Marinho <cadumsantos@hotmail.com>
2022-03-02 15:03:49 -0300

    Static na principal

---
+++ src/application/Program.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static final double IOF = 0.06;
+	
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		System.out.print("What is the dollar price? ");
+		double price = sc.nextDouble();
+		System.out.print("How many dollars will be bought? ");
+		double quantity = sc.nextDouble();
+		
+		double conv = priceConverter(price, quantity);
+		
+		System.out.println("Amount to be paid in reais = " + conv);
+		
+		sc.close();
+	}
+	
+		public static double priceConverter(double price, double quantity) {
+			return price * quantity + price * quantity * IOF;
+		}
+		
+
+}
